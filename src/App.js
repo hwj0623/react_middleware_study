4645c9dbaa728806aa3b7eee4deba0c52bf50a2a
hwj0623 <hwj0623@naver.com>
2019-02-18 01:03:01 +0900

    ch15. init setting

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as counterActions from './modules/counter';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        const { CounterActions, number } = this.props;
+
+
+        return (
+            <div>
+                <h1>{number}</h1>
+                <button onClick={CounterActions.increment}>+</button>
+                <button onClick={CounterActions.decrement}>-</button>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default connect(
+    (state) => ({
+        number: state.counter
+    }),
+    (dispatch) => ({
+        CounterActions: bindActionCreators(counterActions, dispatch)
+    })
+)(App);

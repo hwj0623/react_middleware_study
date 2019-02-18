dff8a5ccd32ce63418fe47b2d993d4798cc2a917
hwj0623 <hwj0623@naver.com>
2019-02-18 23:16:49 +0900

    Ch.18 백엔드 기본 세팅 (2) - koa fwk 기본 세팅

---
+++ blog/blog-backend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-unused-vars":1,
+        "comma-dangle":0,
+        "eol-last":0,
+        "no-console":0
+    }
+};

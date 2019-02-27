c6ac65b8be9459d23b78c9d9ca745a043787609c
hwj0623 <hwj0623@naver.com>
2019-02-27 23:07:23 +0900

    Ch 20.5 EditorHeader, PreviewPane, EditorPane 추가 및 마크다운 페이지 적용

---
+++ blog/blog-frontend/src/components/editor/EditorPane/EditorPane.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import styles from './EditorPane.scss';
+import classNames from 'classnames/bind';
+
+import CodeMirror from 'codemirror';
+
+import 'codemirror/mode/markdown/markdown';//mark down 문법 색상
+//mark down 내부에 들어가는 코드 색상
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/jsx/jsx';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/shell/shell';
+//
+// //CodeMirror 를 위한 CSS 스타일
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
+
+
+const cx = classNames.bind(styles);
+
+class EditorPane extends Component {
+
+    editor = null
+    codeMirror = null
+    initializeEditor = () => {
+        this.codeMirror = CodeMirror(this.editor, {
+            mode: 'markdown',
+            theme: 'monokai',
+            lineNumbers: true,
+            lineWrapping: true
+
+
+        })
+    }
+
+
+    componentDidMount() {
+        this.initializeEditor();
+    };
+
+    render() {
+        return (
+            <div className={cx('editor-pane')}>
+                <input className={cx('title')} placeholder="제목을 입력하세요" name="title"/>
+                <div className={cx('code-editor')} ref={ref => this.editor=ref}></div>
+                <div className={cx('tags')}>
+                    <div className={cx('description')}> 태그</div>
+                    <input name="tags" placeholder="태그를 입력하세요 (, 로 구분)"/>
+                </div>
+            </div>
+        );
+    };
+};
+
+export default EditorPane;
